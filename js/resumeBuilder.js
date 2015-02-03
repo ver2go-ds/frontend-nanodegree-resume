f41bac75f1d2844b03692bb4647e404c27f0982c
bob <bob@burger.com>
2015-02-03 06:16:31 -0800

    Lesson 1 - append - prepend

---
+++ js/resumeBuilder.js
@@ -0,0 +1,7 @@
+//$("#main").append("Bob Sinatra");
+
+var awesomeThoughts = "My name is bob and I am awesome!";
+
+var funThoughts = awesomeThoughts.replace("awesome","fun");
+
+$("#main").append(funThoughts); //jquery main id
